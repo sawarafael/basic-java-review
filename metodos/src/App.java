2da37db644a639b95f0ff6a77d7c85a0d35f4c68
Rafael Soares <sawarafael@gmail.com>
2023-11-18 15:35:56 -0300

    feat: add methods reviewed

--- metodos/src/App.java
+++ metodos/src/App.java
@@ -1,5 +1,58 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Sempre seguir com o CammelCase
+
+        // METODOS
+
+        int a = 3;
+        int b = 4;
+
+        int c = somar(a, b);
+        int d = subtrair(a, b);
+        int e = multiplicar(a, b);
+        int f = dividir(a, b);
+        int g = resto(a, b);
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Ligado: " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
     }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
+    public static int resto(int a, int b) {
+        return a % b;
+    }
+
+    public static void imprimirTexto(String texto) {
+        System.out.println(texto);
+    }
+
+    public static void imprimirNumero(int numero) {
+        System.out.println(numero);
+    }
+
 }
---
+++ metodos/src/SmartTv.java
@@ -0,0 +1,6 @@
+public class SmartTv {
+    boolean ligada = true;
+
+    int canal = 1;
+    int volume = 25;
+}
