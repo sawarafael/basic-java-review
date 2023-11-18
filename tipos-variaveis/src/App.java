c0b1e10bbc6dd254ccf8db87cfe14369cc3f7f0d
Rafael Soares <sawarafael@gmail.com>
2023-11-18 14:06:19 -0300

    chore: add more studies to variables types

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -9,6 +9,21 @@ public class App {
         float pi = 3.14F; // 4 bytes
         double preco = 12.5; // 8 bytes
 
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        char letra = 'a';
+
+        System.out.println(idade);
+        System.out.println(anoFabricacao);
+        System.out.println(anoModelo);
+        System.out.println(matricula);
+        System.out.println(pi);
+        System.out.println(preco);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(letra);
+
         // JAVA É FORTEMENTE TIPADO
 
         short numeroCurto = 1;
@@ -16,5 +31,19 @@ public class App {
 
         // FINAL DETERMINA O VALOR FINAL DE UMA VARIÁVEL, ENQUANTO ELE NÃO RECEBER ESTE
         // PONTO, ELE SEMPRE PODERÁ SER ALTERADO
+
+        System.err.println(numeroNormal);
+
+        final int numeroFinal = 1;
+        System.err.println(numeroFinal);
+
+        // numeroFinal = 2;
+
+        String nome = "Joaquim";
+        String nome2 = new String("Joaquim");
+
+        System.err.println(nome);
+        System.err.println(nome2);
+
     }
 }
